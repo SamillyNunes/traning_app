9284e5cdd6bc1651fa9cd853220a051cabca6a6c
Samilly <samillynunes19@gmail.com>
2021-11-01 21:03:27 -0300

    Settings files

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color homePageBackground = Color(0xFFfbfcff);
+  static const Color gradientFirst = Color(0xff0f17ad);
+  static const Color gradientSecond = Color(0xFF6985e8);
+  static const Color homePageTitle = Color(0xFF302f51);
+  static const Color homePageSubtitle = Color(0xFF414160);
+  static const Color homePageDetail = Color(0xFF6588f4);
+  static const Color homePageIcons = Color(0xFF3b3c5c);
+  static const Color homePageContainerTextSmall = Color(0xFFf4f5fd);
+  static const Color homePageContainerTextBig = Color(0xFFffffff);
+  static const Color homePagePlanColor = Color(0xFFa2a2b1);
+  static const Color secondPageTopIconColor = Color(0xFFb7bce8);
+  static const Color secondPageTitleColor = Color(0xFFfefeff);
+  static const Color secondPageContainerGradient1stColor = Color(0xFF5564d8);
+  static const Color secondPageContainerGradient2ndColor = Color(0xFF6279dc);
+  static const Color secondPageIconColor = Color(0xFFfafafe);
+  static const Color loopColor = Color(0xFF6d8dea);
+  static const Color setsColor = Color(0xFF9999a9);
+  static const Color circuitsColor = Color(0xFF2f2f51);
+}
---
+++ lib/core/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages {
+  static const String cardImg = 'assets/images/card.jpg';
+  static const String cardPngImg = 'assets/images/card.png';
+  static const String ex1Img = 'assets/images/ex1.png';
+  static const String ex2Img = 'assets/images/ex2.png';
+  static const String ex3Img = 'assets/images/ex3.png';
+  static const String figureImg = 'assets/images/figure.png';
+  static const String figure1Img = 'assets/images/figure1.png';
+  static const String squat1Img = 'assets/images/squat1.jpg';
+  static const String squat2Img = 'assets/images/squat2.jpg';
+  static const String squat3Img = 'assets/images/squat3.jpg';
+  static const String squat4Img = 'assets/images/squat4.jpg';
+}
